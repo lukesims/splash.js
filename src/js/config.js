db6591d925775f248d124ab59956f3c24ee88c0f
Luke Sims <ls@lukesims.uk>
2018-08-24 15:04:45 +0100

    Update config - Remove disabled class, just use attributes - Add new options for swapping z-indexes

--- src/js/config.js
+++ src/js/config.js
@@ -1,21 +1,33 @@
 // The default configuration for the library.
 //   Separated into another file so it can be pulled into tests.
 export default {
+  // Whether the click effect is enabled
   click: true,
+  // Whether the focus effect is enabled
   focus: false,
+  // Whether the hover effect is enabled
   hover: false,
+  // Whether to swap the z-index of the waves and content
+  // When false, the waves are _above_ the element's content
+  swap: false,
+  // Whether click waves should wait for the `mouseup` event to fade out.
+  // When false, wave fades out after fully covering the splash element.
   waitForMouseup: false,
+  // html/css class names employed by the library
   class: {
+    // For the base splash element
     base: 'splash',
+    swap: 'splash-swap',
+    // Wave and content wrappers
+    waves: 'splash-waves',
+    wrap: 'splash-wrap',
+    // For the wave elements
     clickWave: 'splash-click',
     clickOut: 'splash-click-out',
     focusWave: 'splash-focus',
     focusOut: 'splash-focus-out',
-    disabled: 'disabled',
     hide: 'splash-hide',
     hoverWave: 'splash-hover',
     hoverOut: 'splash-hover-out',
-    waves: 'splash-waves',
-    wrap: 'splash-wrap',
   },
 };
